e4a9c27f701f2e4be341c5f4324efd387b6914f2
drbobdugan <bdugan@stonehill.edu>
2015-11-06 10:06:36 -0500

    Fixed syntax error

--- LongRunningTaskRecordAudioExample/AppDelegate.m
+++ LongRunningTaskRecordAudioExample/AppDelegate.m
@@ -29,7 +29,7 @@
     NSLog(@"%s", __PRETTY_FUNCTION__);
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)applicatio n
+- (void)applicationDidEnterBackground:(UIApplication *)application
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
